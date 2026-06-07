53464c8ad4503b3b0f7762b23d6218db8d97af8f
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2026-06-07 12:54:22 +0000

    fix: Refactor DEBUG and ALLOWED_HOSTS settings to use os.getenv for environment variable retrieval

--- ScrubiMail-BE/backend/settings.py
+++ ScrubiMail-BE/backend/settings.py
@@ -116,9 +116,9 @@ OAUTH_PROVIDERS_CONFIG = {
 }
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.get_inheritable("DEBUG", "True").lower() == "true"
+DEBUG = os.getenv("DEBUG", "True").lower() == "true"
 
-ALLOWED_HOSTS = os.get_inheritable("ALLOWED_HOSTS", "*")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*")
 
 
 # Application definition
